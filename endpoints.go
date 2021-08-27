dfe3e789d2d832cbaea51894ba17c60b204705ee
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-27 18:09:40 +0200

    add Login endpoint

--- endpoints.go
+++ endpoints.go
@@ -3,8 +3,9 @@ package unaswrappergo
 type endpointEnumType string
 
 const (
-	GetNewsletter = "https://api.unas.eu/shop/getNewsletter"
-	SetNewsletter = "https://api.unas.eu/shop/setNewsletter"
-	GetProduct    = "https://api.unas.eu/shop/getProduct"
-	SetProduct    = "https://api.unas.eu/shop/setProduct"
+	GetNewsletter endpointEnumType = "https://api.unas.eu/shop/getNewsletter"
+	SetNewsletter                  = "https://api.unas.eu/shop/setNewsletter"
+	GetProduct                     = "https://api.unas.eu/shop/getProduct"
+	SetProduct                     = "https://api.unas.eu/shop/setProduct"
+	LoginEnd                       = "https://api.unas.eu/shop/login"
 )

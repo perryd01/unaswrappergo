538bf511b71af9e48c27e2fe16d982f48b705c80
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-27 19:09:06 +0200

    add product related endpoint urls

--- endpoints.go
+++ endpoints.go
@@ -5,7 +5,9 @@ type endpointEnumType string
 const (
 	GetNewsletter endpointEnumType = "https://api.unas.eu/shop/getNewsletter"
 	SetNewsletter                  = "https://api.unas.eu/shop/setNewsletter"
+	LoginEnd                       = "https://api.unas.eu/shop/login"
 	GetProduct                     = "https://api.unas.eu/shop/getProduct"
 	SetProduct                     = "https://api.unas.eu/shop/setProduct"
-	LoginEnd                       = "https://api.unas.eu/shop/login"
+	GetProductDB                   = "https://api.unas.eu/shop/getProductDB"
+	SetProductDB                   = "https://api.unas.eu/shop/setProductDB"
 )

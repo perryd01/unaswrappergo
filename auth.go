2516f3c856dde305b501ab762d8988b266ba2c9f
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-27 18:09:27 +0200

    auth fix

--- auth.go
+++ auth.go
@@ -5,36 +5,40 @@ import (
 	"encoding/xml"
 	"errors"
 	"io/ioutil"
+	"mime"
 	"net/http"
 )
 
-type LoginAPIRequest struct {
-	Params LoginAPIParams `xml:"Params"`
+type loginAPIRequest struct {
+	Params loginAPIParams `xml:"Params"`
 }
 
-type LoginAPIParams struct {
+type loginAPIParams struct {
 	APIKey string `xml:"ApiKey"`
 }
 
-type LoginAPIResponse struct {
-	Login Login `xml:"Login"`
+type loginAPIResponse struct {
+	Login login `xml:"Login"`
 }
 
-type Login struct {
+// Struct which contains data for request authentication
+type login struct {
 	Token       string      `xml:"Login>Token"`
 	Expire      string      `xml:"Login>Expire"`
-	Permissions Permissions `xml:"Login>Permissions"`
+	Permissions permissions `xml:"Login>Permissions"`
 	Status      string      `xml:"Login>Status"`
 }
 
-type Permissions struct {
+// Permissions for allowed methods towards Unas
+type permissions struct {
 	Permission []string `xml:"Permission"`
 }
 
-type AuthPassRequest struct {
+type authPassRequest struct {
 	Auth Auth `xml:"Auth"`
 }
 
+// Struct for login with user:pass
 type Auth struct {
 	Username      string `xml:"Username"`
 	PasswordCrypt string `xml:"PasswordCrypt"`
@@ -45,7 +49,7 @@ type Auth struct {
 const loginEndpoint = "https://api.unas.eu/shop/login"
 
 func AuthwithAPIKey(apikey string) (*UnasObject, error) {
-	payload := LoginAPIRequest{Params: LoginAPIParams{APIKey: apikey}}
+	payload := loginAPIRequest{Params: loginAPIParams{APIKey: apikey}}
 	xmlpayload, err := xml.Marshal(payload)
 	if err != nil {
 		return nil, err
@@ -55,12 +59,12 @@ func AuthwithAPIKey(apikey string) (*UnasObject, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/xml")
+	req.Header.Set("Content-Type", mime.TypeByExtension(".xml"))
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -68,7 +72,7 @@ func AuthwithAPIKey(apikey string) (*UnasObject, error) {
 		return nil, errors.New("unsuccessful post")
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
-	xmlresponse := LoginAPIResponse{}
+	xmlresponse := loginAPIResponse{}
 	err = xml.Unmarshal(body, &xmlresponse)
 	if err != nil {
 		return nil, err
@@ -82,7 +86,7 @@ func AuthwithAPIKey(apikey string) (*UnasObject, error) {
 }
 
 func AuthwithPass(a Auth) (*UnasObject, error) {
-	xmlpayload, err := xml.Marshal(AuthPassRequest{Auth: a})
+	xmlpayload, err := xml.Marshal(authPassRequest{Auth: a})
 	if err != nil {
 		return nil, err
 	}
@@ -90,12 +94,12 @@ func AuthwithPass(a Auth) (*UnasObject, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/xml")
+	req.Header.Set("Content-Type", mime.TypeByExtension(".xml"))
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -103,7 +107,7 @@ func AuthwithPass(a Auth) (*UnasObject, error) {
 		return nil, errors.New("unsuccessful post")
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
-	xmlresponse := LoginAPIResponse{}
+	xmlresponse := loginAPIResponse{}
 	err = xml.Unmarshal(body, &xmlresponse)
 	if err != nil {
 		return nil, err

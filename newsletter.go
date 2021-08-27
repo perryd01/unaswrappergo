2f06798b80cbeacb462eb954b01c25ac789c8a9e
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-27 23:05:38 +0200

    newsletter with comments

--- newsletter.go
+++ newsletter.go
@@ -1,89 +1,99 @@
 package unaswrappergo
 
-import (
-	"encoding/xml"
-)
+import "encoding/xml"
 
-type GetNewsletterRequest struct {
-	Params NewsletterParams `xml:"Params"`
+// GetNewsletterParams Params to set for querying newsletter subscribers
+// https://unas.hu/tudastar/api/newsletter#getnewsletter-keres
+type GetNewsletterParams struct {
+	Type      string        `xml:"Params>Type,omitempty"`
+	Auth      string        `xml:"Params>Auth,omitempty"` //TODO bool
+	TimeStart UnasTimeStamp `xml:"Params>TimeStart,omitempty"`
+	TimeEnd   UnasTimeStamp `xml:"Params>TimeEnd,omitempty"`
 }
 
-// igénylő típusa, lehetséges értékek:
-// 	customer - vásárló;
-//	subscriber - csak igénylő
-type newsletterIdentityType string
-
-const (
-	Customer   = "customer"
-	Subscriber = "subscriber"
-)
-
-//a hírlevél igénylő visszaigazolta a feliratkozását, "yes" vagy "1" érték: feliratkozás visszaigazolva; "no" vagy "0" érték: feliratkozás nincs visszaigazolva
-type newsletterJoinAcceptType string
-
-const (
-	Yes = "yes"
-	No  = "no"
-)
-
-type GetNewsletterResponse struct {
-	Subscribers newsletterSubscriberList `xml:"Subscribers"`
+type getNewsletterResponse struct {
+	Subscribers []*NewsletterSubscriber `xml:"Subscribers"`
 }
 
-type newsletterSubscriberList struct {
-	Subscriber []newsletterSubscriber `xml:"Subscriber"`
+// NewsletterSubscriber One subscriber of the newsletter
+type NewsletterSubscriber struct {
+	Email      string        `xml:"Email,omitempty"`
+	Type       string        `xml:"Type,omitempty"`
+	Time       UnasTimeStamp `xml:"Time,omitempty"`
+	Name       string        `xml:"Name,omitempty"`
+	Address    string        `xml:"Address,omitempty"`
+	Lang       string        `xml:"Lang,omitempty"`
+	Authorized string        `xml:"Authorized,omitempty"` //TODO bool
 }
 
-type newsletterSubscriber struct {
-	Email      *string `xml:"Email,omitempty"`
-	Type       *string `xml:"Type,omitempty"`
-	Time       *string `xml:"Time,omitempty"`
-	Name       *string `xml:"Name,omitempty"`
-	Address    *string `xml:"Address,omitempty"`
-	Lang       *string `xml:"Lang,omitempty"`
-	Authorized *string `xml:"Authorized,omitempty"`
+type SetNewsletterSubscriberParams struct {
+	Action string `xml:"Action,omitempty"`
+	Email string `xml:"Email,omitempty"`
+	Name string `xml:"Name,omitempty"`
 }
 
-type NewsletterParams struct {
-	Type      newsletterIdentityType `xml:"Type"`
-	Auth      string                 `xml:"Auth"`
-	TimeStart *string                `xml:"TimeStart"`
-	TimeEnd   *string                `xml:"TimeEnd"`
+type SetNewsletterSubscriberStatus struct {
+	Action string `xml:"Action,omitempty"`
+	Email string `xml:"Email,omitempty"`
+	Name string `xml:"Name,omitempty"`
+	Status string `xml:"Status,omitempty"`
 }
 
-func (uo *UnasObject) getNewsletter(np *NewsletterParams) (*newsletterSubscriberList, error) {
-	getnwr := GetNewsletterRequest{Params: *np}
-	b, err := xml.Marshal(getnwr)
-	if err != nil {
-		return nil, err
-	}
-	body, err := uo.makeRequest(endpointEnumType(GetNewsletter), b)
+// SetNewsletterRequest
+// https://unas.hu/tudastar/api/newsletter#setnewsletter-keres
+type setNewsletterRequest struct {
+	Subscribers []*SetNewsletterSubscriberParams `xml:"Subscribers"`
+}
+// SetNewsletterResponse
+// https://unas.hu/tudastar/api/newsletter#setnewsletter-valasz
+type setNewsletterResponse struct {
+	Subscribers []*SetNewsletterSubscriberStatus `xml:"Subscribers"`
+}
+
+// GetNewsletter Queries newsletter subscribers of the webshop
+// https://unas.hu/tudastar/api/newsletter#getnewsletter-funkcio
+func (uo UnasObject) GetNewsletter(params *GetNewsletterParams) ([]*NewsletterSubscriber, error) {
+	body, err := xml.Marshal(params)
 	if err != nil {
 		return nil, err
 	}
 
-	nlsl := newsletterSubscriberList{}
-
-	err = xml.Unmarshal(body, &nlsl)
+	resp, err := uo.makeRequest(GetNewsletter, body)
 	if err != nil {
 		return nil, err
 	}
 
-	return &nlsl, nil
+	gnlresp := getNewsletterResponse{}
+
+	err = xml.Unmarshal(resp, &gnlresp)
+	if err != nil {
+		return nil, err
+	}
+
+	return gnlresp.Subscribers, nil
 }
 
-type setNewsletterSubscriberList struct {
-	Subscribers setNewsletterSubscribers `xml:"Subscribers"`
-}
+// SetNewsletter Add/Modify/Delete data of newsletter subscribers
+// https://unas.hu/tudastar/api/newsletter#setnewsletter-funkcio
+func (uo UnasObject) SetNewsletter(subscribers []*SetNewsletterSubscriberParams) ([]*SetNewsletterSubscriberStatus, error) {
+	snreq := setNewsletterRequest{Subscribers: subscribers}
 
-type setNewsletterSubscribers struct {
-	Subscriber []setNewsletterSubscriber `xml:"Subscriber"`
-}
+	b, err := xml.Marshal(snreq)
+	if err != nil {
+		return nil, err
+	}
 
-type setNewsletterSubscriber struct {
-	Action string `xml:"Action"`
-	Email  string `xml:"Email"`
-	Name   string `xml:"Name"`
-}
+	r, err := uo.makeRequest(SetNewsletter, b)
+	if err != nil {
+		return nil, err
+	}
 
-//func (uo *UnasObject) setNewsletter()
+	snresp := setNewsletterResponse{}
+
+	err = xml.Unmarshal(r, snresp)
+	if err != nil {
+		return nil, err
+	}
+
+	return snresp.Subscribers, nil
+}

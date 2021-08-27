2f5fbe91c524ea8b961a4f26e546799346d41627
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-27 18:54:41 +0200

    setProduct without proper productStatus Enums

--- product_set.go
+++ product_set.go
@@ -1,9 +1,48 @@
 package unaswrappergo
 
-type SetProductRequestParams struct {
-	Products []Product
+import "encoding/xml"
+
+type setProductRequestParams struct {
+	Products []*Product `xml:"Product`
 }
 
-type SetProductResponse struct{}
+type setProductRequest struct {
+	P setProductRequestParams `xml:"Products"`
+}
 
-func (uo UnasObject) SetProduct(products []*Product)
+type setProductResponse struct {
+	setProductStatuses `xml:"Products"`
+}
+
+type setProductStatuses struct {
+	Products []*productStatus `xml:"Product"`
+}
+
+type productStatus struct {
+	ID     string `xml:"Id,omitempty"`
+	Sku    string `xml:"Sku,omitempty"`
+	Action string `xml:"Action,omitempty"`
+	Status string `xml:"Status,omitempty"`
+	Error  string `xml:"Error,omitempty"`
+}
+
+func (uo UnasObject) SetProduct(products []*Product) ([]*productStatus, error) {
+	body := setProductRequest{P: setProductRequestParams{Products: products}}
+	b, err := xml.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	r, err := uo.makeRequest(endpointEnumType(SetProduct), b)
+	if err != nil {
+		return nil, err
+	}
+
+	spResponse := setProductResponse{}
+
+	err = xml.Unmarshal(r, &spResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return spResponse.setProductStatuses.Products, nil
+}

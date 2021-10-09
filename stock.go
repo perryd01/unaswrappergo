24e107f7c27ebedb73616350ffd5c69a60004a8a
Dániel Miklós <daniel.miklos0@gmail.com>
2021-10-09 16:58:45 +0200

    add stocks

--- stock.go
+++ stock.go
@@ -1,26 +1,91 @@
 package unaswrappergo
 
+import "encoding/xml"
+
 type GetStockParams struct {
-	Text       string `xml:",chardata"`
-	ID         string `xml:"Id"`
-	Sku        string `xml:"Sku"`
-	Variant1   string `xml:"Variant1"`
-	Variant2   string `xml:"Variant2"`
-	Variant3   string `xml:"Variant3"`
-	LimitStart string `xml:"LimitStart"`
-	LimitNum   string `xml:"LimitNum"`
+	XMLName    xml.Name `xml:"Params"`
+	ID         string   `xml:"Id"`
+	Sku        string   `xml:"Sku"`
+	Variant1   string   `xml:"Variant1"`
+	Variant2   string   `xml:"Variant2"`
+	Variant3   string   `xml:"Variant3"`
+	LimitStart string   `xml:"LimitStart"`
+	LimitNum   string   `xml:"LimitNum"`
 }
 
-type getStockRequest struct {
-	Params GetStockParams `xml:"Params"`
+type SetStockParams struct {
+	XMLName xml.Name `xml:"Products"`
+	Product []struct {
+		Action string `xml:"Action,omitempty"`
+		Sku    string `xml:"Sku,omitempty"`
+		Stocks struct {
+			Stock []struct {
+				Qty      string `xml:"Qty,omitempty"`
+				Price    string `xml:"Price,omitempty"`
+				Variants struct {
+					Variant []string `xml:"Variant,omitempty"`
+				} `xml:"Variants,omitempty"`
+			} `xml:"Stock,omitempty"`
+		} `xml:"Stocks,omitempty"`
+	} `xml:"Product"`
+}
+
+type SetStockProductStatus struct {
+	ID     string `xml:"Id"`
+	Sku    string `xml:"Sku"`
+	Action string `xml:"Action"`
+	Status string `xml:"Status"`
+	Error  string `xml:"Error"`
+}
+
+type setStockResponse struct {
+	XMLName xml.Name                 `xml:"Products"`
+	Stocks  []*SetStockProductStatus `xml:"Product"`
 }
 
 type getStockResponse struct {
-	Products GetStockResponseProduct `xml:"Products"`
+	XMLName xml.Name            `xml:"Products"`
+	Product []*ProductStockData `xml:"Product,omitempty"`
 }
 
-type GetStockResponseProduct struct {
+type ProductStockData struct {
+	Sku   string `xml:"Sku,omitempty"`
+	Stock []struct {
+		Variant []string `xml:"Variants>Variant"`
+		Qty     string   `xml:"Qty"`
+	} `xml:"Stocks>Stock"`
 }
 
-// func (uo UnasObject) getStock(params GetStockParams) ([]*GetStockResponseProduct, error) {}
-// func (uo UnasObject) setStock()                                                          {}
+func (uo UnasObject) getStock(params GetStockParams) ([]*ProductStockData, error) {
+	bodyMarshaled, err := xml.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := uo.makeRequest(GetStock, bodyMarshaled)
+	if err != nil {
+		return nil, err
+	}
+	var stocks = getStockResponse{}
+	err = xml.Unmarshal(resp, &stocks)
+	if err != nil {
+		return nil, err
+	}
+	return stocks.Product, nil
+}
+
+func (uo UnasObject) setStock(params *SetStockParams) ([]*SetStockProductStatus, error) {
+	bodyMarshaled, err := xml.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := uo.makeRequest(SetStock, bodyMarshaled)
+	if err != nil {
+		return nil, err
+	}
+	var stocks = setStockResponse{}
+	err = xml.Unmarshal(resp, &stocks)
+	if err != nil {
+		return nil, err
+	}
+	return stocks.Stocks, nil
+}

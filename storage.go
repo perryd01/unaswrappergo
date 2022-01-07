034fee390b4747fde903ceae09d9934ab614c176
Dániel Miklós <daniel.miklos0@gmail.com>
2022-01-07 18:03:42 +0100

    add setStorage

--- storage.go
+++ storage.go
@@ -2,10 +2,26 @@ package unaswrappergo
 
 import "encoding/xml"
 
+type getStorageType string
+
+const (
+	StorageTypeAll    = "all"
+	StorageTypeFile   = "file"
+	StorageTypeFolder = "folder"
+)
+
+type getStorageGetInfo string
+
+const (
+	StorageGetInfoNo   = "no"
+	StorageGetInfoYes  = "yes"
+	StorageGetInfoOnly = "only"
+)
+
 type GetStorageParams struct {
-	Type    string `xml:"Params>Type,omitempty"`    // TODO enum
-	GetInfo string `xml:"Params>GetInfo,omitempty"` //TODO enum
-	Folder  string `xml:"Params>Folder,omitempty"`
+	Type    getStorageType    `xml:"Params>Type,omitempty"`
+	GetInfo getStorageGetInfo `xml:"Params>GetInfo,omitempty"`
+	Folder  string            `xml:"Params>Folder,omitempty"`
 }
 
 type getStorageResponse struct {
@@ -40,4 +56,54 @@ func (uo *UnasObject) GetStorage(params *GetStorageParams) ([]*StorageItem, erro
 	}
 
 	return gStorageResponse.StorageItems, nil
+}
+
+type setStorageItemAction string
+
+const (
+	CreateFolder setStorageItemAction = "create_folder"
+	DeleteFolder setStorageItemAction = "create_folder"
+	UploadFile   setStorageItemAction = "upload_file"
+	DeleteFile   setStorageItemAction = "delete_file"
+)
+
+type setStorageItemRequest struct {
+	XMLName     xml.Name         `xml:"StorageItems"`
+	StorageItem []SetStorageItem `xml:"StorageItem"`
+}
+
+type SetStorageItem struct {
+	Action setStorageItemAction `xml:"Action,omitempty"`
+	Folder string               `xml:"Folder,omitempty"`
+	Url    string               `xml:"Url,omitempty"`
+	Name   string               `xml:"Name,omitempty"`
+}
+
+type setStorageResponse struct {
+	XMLName     xml.Name                 `xml:"StorageItems"`
+	StorageItem []SetStorageResponseItem `xml:"StorageItem"`
+}
+
+type SetStorageResponseItem struct {
+	Action string `xml:"Action"`
+	Status string `xml:"Status"`
+}
+
+//SetStorage https://unas.hu/tudastar/api/storage#setstorage-funkcio
+func (uo *UnasObject) SetStorage(items []SetStorageItem) ([]SetStorageResponseItem, error) {
+	b, err := xml.Marshal(setStorageItemRequest{StorageItem: items})
+	if err != nil {
+		return nil, err
+	}
+	respBody, err := uo.makeRequest(SetStorage, b)
+	if err != nil {
+		return nil, err
+	}
+
+	sStorageResponse := setStorageResponse{}
+	err = xml.Unmarshal(respBody, &sStorageResponse)
+	if err != nil {
+		return nil, err
+	}
+	return sStorageResponse.StorageItem, nil
 }

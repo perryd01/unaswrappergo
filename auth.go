75f0ef33c3b1ed2314acfa5f91671e3884a67647
Dániel Miklós <daniel.miklos0@gmail.com>
2022-01-07 16:58:00 +0100

    test auth and remove auth by username

--- auth.go
+++ auth.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/xml"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"mime"
@@ -36,14 +35,6 @@ type Auth struct {
 	PasswordCrypt string `xml:"PasswordCrypt"`
 	ShopID        string `xml:"ShopId"`
 	AuthCode      string `xml:"AuthCode"`
-}
-
-type authPassRequest struct {
-	XMLName       xml.Name `xml:"Auth"`
-	Username      string   `xml:"Username"`
-	PasswordCrypt string   `xml:"PasswordCrypt"`
-	ShopId        string   `xml:"ShopId"`
-	AuthCode      string   `xml:"AuthCode"`
 }
 
 // AuthwithAPIKey Authenticating using an API key.
@@ -79,56 +70,6 @@ func AuthwithAPIKey(apikey string) (*UnasObject, error) {
 		_ = Body.Close()
 	}(resp.Body)
 
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("unsuccessful post")
-	}
-
-	xmlresponse := login{}
-	err = xml.Unmarshal(body, &xmlresponse)
-	if err != nil {
-		return nil, err
-	}
-
-	uo := UnasObject{
-		Login: xmlresponse,
-	}
-
-	return &uo, nil
-}
-
-// AuthwithPass Authenticating using a User:Pass combo.
-// https://unas.hu/tudastar/api/authorization#felhasznalonev-alapu-azonositas
-
-func AuthwithPass(a Auth) (*UnasObject, error) {
-	xmlpayload, err := xml.Marshal(authPassRequest{XMLName: xml.Name{Local: "Auth"}, Username: a.Username, ShopId: a.ShopID, PasswordCrypt: a.PasswordCrypt, AuthCode: a.AuthCode})
-	if err != nil {
-		return nil, err
-	}
-
-	reqBuf := bytes.NewBuffer([]byte(xml.Header))
-	_, err = reqBuf.Write(xmlpayload)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", string(LoginEndPoint), reqBuf)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", mime.TypeByExtension(".xml"))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
-
-	fmt.Println(resp.StatusCode)
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	if resp.StatusCode != http.StatusOK {
